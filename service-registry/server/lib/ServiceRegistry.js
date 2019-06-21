28b166460b3b1c5eaf0508d2959312cfded8258e
Mir HB Rahman <ahadcr0@gmail.com>
2019-06-22 01:30:30 +0600

    Added service cleanup function

--- service-registry/server/lib/ServiceRegistry.js
+++ service-registry/server/lib/ServiceRegistry.js
@@ -1,11 +1,22 @@
+const semver = require('semver');
+
 class ServiceRegistry {
 	constructor(log) {
 		this.log = log;
 		this.services = {};
-		this.timeout = 30;
+		this.timeout = 20;
+	}
+
+	get(name, version) {
+		this.cleanup();
+		const candidates = Object.values(this.services)
+			.filter(service => service.name === name && semver.satisfies(service.version, version));
+
+		return candidates[Math.floor(Math.random() * candidates.length)];
 	}
 
 	register(name, version, ip, port) {
+		this.cleanup();
 		const key = name + version + ip + port;
 
 		if (!this.services[key]) {
@@ -29,6 +40,16 @@ class ServiceRegistry {
 		const key = name + version + ip + port;
 		delete this.services[key];
 		return key;
+	}
+
+	cleanup() {
+		const now = Math.floor(new Date() / 1000);
+		Object.keys(this.services).forEach(key => {
+			if (this.services[key].timestamp + this.timeout < now) {
+				delete this.services[key];
+				this.log.debug(`Remove service ${key}`);
+			}
+		});
 	}
 }
 
